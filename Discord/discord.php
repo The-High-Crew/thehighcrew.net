51b31c24d408a56451e6fa77a7aa22af925bb33d
Jeremy Zijlemans <jeremyzijlemans@gmail.com>
2021-06-26 22:16:58 +0200

    Adding the discord to the site

---
+++ Discord/discord.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SionHub</title>
+    <link href="../components/icons/sion-net-globe.ico" rel="shortcut icon" type="image/x-icon">
+    <link rel="stylesheet" href="css/discord.css">
+</head>
+<body>
+<div class="wrap">
+    <div class="name--header">
+        <div class="name"></div>
+
+        <div class="discord">
+            <a href="https://discordapp.com/" class="discord--link" target='_blank'>Powered By: <img src="https://discordapp.com/assets/4f004ac9be168ac6ee18fc442a52ab53.svg" alt="" /></a>
+        </div>
+    </div>
+    <div class="join--header">
+        <div class="sion">Sion Development</div>
+
+        <div class="join">
+            <a href="https://discordapp.com/" class="join--link" target='_blank'>Join here: <img src="../components/icons/sion-net-globe.ico" width="50px" alt="" /></a>
+        </div>
+    </div>
+    <div class="channels">
+        <div class="channels--header">Channels</div>
+        <div class="channels--body"></div>
+    </div>
+    <div class="users">
+        <div class="users--header">Online Users</div>
+        <div class="users--body"></div>
+    </div>
+</div>
+<script src="js/discord.js"></script>
+</body>
+<footer>
+</footer>
+</html>
