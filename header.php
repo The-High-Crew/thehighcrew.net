c1c05db9f62db23e73a84b795ad3ee6018d4c038
Jeremy Zijlemans <jeremyzijlemans@gmail.com>
2021-06-26 23:03:40 +0200

    Adding the base

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('ENABLE_HEADER')) {
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>thehighcrew.net</title>
+    <link href="#" rel="shortcut icon" type="image/x-icon">
+    <link href="#" rel="stylesheet" type="text/css">
+    <link href="#" rel="script" type="text/js">
+</head>
+<body>
+
