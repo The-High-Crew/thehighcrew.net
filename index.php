99d0be2e335e5ad96f3b82496d6a7e073c47d41a
Jeremy Zijlemans <jeremyzijlemans@gmail.com>
2021-07-04 00:34:48 +0200

    moved the site to a subdomain and started preping the rework of the site

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>TheHighCrew</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" type="image/png" href="#" />
+    <link rel="icon" type="image/png" href="/misc/logo/Logo.png" />
 <body>
 </body>
 </html>
